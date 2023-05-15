ca603497b84d1e7a55792e39925565eeaba59a85
msholl <matheus@conetca-soft.com.br>
2023-05-15 13:02:33 -0300

    Criando factory de produto

---
+++ database/factories/ProtudoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProtudoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome' => $this->faker->name(),
+            'descricao' => $this->faker->text(),
+            'preco' => $this->faker->randomFloat(2, 0, 8),
+            'quantidade' => $this->faker->randomNumber(2),
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Produto;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Produto::factory()->count(12)->create();
     }
 }
